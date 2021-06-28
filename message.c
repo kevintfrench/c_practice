7dcd374075aa1b2b05ad8b61688e11448013cf8e
[Kevin <[kfrench@gsinet.net]>
2021-06-28 13:27:45 -0400

    add message

---
+++ message.c
@@ -0,0 +1,19 @@
+/* 
+*Building a message with safe string functions
+*/
+
+#include<stdio.h>
+#include<string.h>
+
+int main(void) {
+	/*
+	*read a name from user
+	*/
+
+	printf("What is your name? ");
+
+	char[40];
+	scanf("%39", name);
+
+	
+}
