75bc6c5573595203d3652b91c5a8904d91d499fc
[Kevin <[kfrench@gsinet.net]>
2021-06-28 13:35:56 -0400

    add output

--- message.c
+++ message.c
@@ -15,5 +15,20 @@ int main(void) {
 	char[40];
 	scanf("%39", name);
 
-	
+	/*
+	*<name> is learning C
+	*/
+
+	char message[100];  /*Destination String*/
+
+	strcpy_s(message, sizeof(message), name );
+	strcpy_s(message, sizeof(message), "is" );
+	strcpy_s(message, sizeof(message), "learning C." );
+
+	/* print out message */
+    puts(message);
+
+    return 0;
+
+
 }
